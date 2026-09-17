75f76d98f9567a2b0ce776ab37855b50cdce47ff
saraehrari <saraehrari2020@gmail.com>
2026-09-17 14:21:56 +0430

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,32 @@
 
 
+
 import { useEffect, useState } from "react";
 import Button from "./component/Button";
 import Dailyquote from "./component/Dailyquote";
 
- function App() {
-  const[quote, setQuote]=useState({});
-useEffect(()=>{
-async function Fetchquotes(){
-  try{
-const res= fetch""
+function App() {
+  const [quote, setQuote] = useState({});
+
+  async function getQuote() {
+    const res = await fetch("https://zenquotes.io/api/random");
+    const data = await res.json();
+
+    setQuote(data[0]);
   }
 
-  catch{
+  useEffect(() => {
+    getQuote();
+  }, []);
 
-
-  }
-
-  finally{
-
-
-  }
-}
-
-
-
-Fetchquotes();
-},[]);
-  return(
+  return (
     <div>
-      <Dailyquote Dailyquote={quote}/>
-    <Button getQuote={getQuote}/>
+      <Dailyquote Dailyquote={quote} />
+      <Button getQuote={getQuote} />
     </div>
-  )
+  );
 }
+
 export default App;
+
 
